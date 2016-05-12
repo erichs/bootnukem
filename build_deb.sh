fb0cd678c7c86783ba35c7dfe350da65c535b7b0
Erich Smith <heartquest@gmail.com>
2016-05-12 15:09:18 -0400

    Produce architecture-independent deb

--- build_deb.sh
+++ build_deb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo aptitude install checkinstall
 version=$(./bootnukem --version)
-sudo checkinstall --pkgname=bootnukem --pkgversion=$version --maintainer=heartquest@gmail.com --type=debian --pkglicense=MIT -y ./install.sh
+sudo checkinstall --pkgname=bootnukem --pkgversion=$version --maintainer=heartquest@gmail.com --type=debian --pkgarch=all --pkglicense=MIT -y ./install.sh
